eb8f97fab650b7aabfef1a5a473a2737c087b25c
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-15 15:58:59 -0300

    cambios

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
